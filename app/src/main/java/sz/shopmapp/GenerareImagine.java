694bce01d5388f35af589b73707f61a05a1fd5e0
zuzuba23 <tavi2303@yahoo.com>
2017-04-15 21:00:18 +0300

    test

--- app/src/main/java/sz/shopmapp/GenerareImagine.java
+++ app/src/main/java/sz/shopmapp/GenerareImagine.java
@@ -8,6 +8,5 @@ public class GenerareImagine extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_generare_imagine);
     }
 }
