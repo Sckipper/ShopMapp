f1c9141a8cd1eb96cf7e791453eacc098de24904
Sckipper <yo_da_daniel@yahoo.com>
2017-04-15 21:02:04 +0300

    modificare GenerareImagine

--- app/src/main/java/sz/shopmapp/GenerareImagine.java
+++ app/src/main/java/sz/shopmapp/GenerareImagine.java
@@ -8,5 +8,6 @@ public class GenerareImagine extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(new CustomView(this));
     }
 }
