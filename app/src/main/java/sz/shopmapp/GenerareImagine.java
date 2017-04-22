5b80d32702fd5f85718ddafad83a68ab380ab5f6
Sckipper <yo_da_daniel@yahoo.com>
2017-04-22 19:23:00 +0300

    changed view

--- app/src/main/java/sz/shopmapp/GenerareImagine.java
+++ app/src/main/java/sz/shopmapp/GenerareImagine.java
@@ -2,12 +2,18 @@ package sz.shopmapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class GenerareImagine extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        //Log.d("Android: " , imgView.toString());
+        //imgView.setImageResource(R.drawable.magazin);
         setContentView(new CustomView(this));
     }
 }
