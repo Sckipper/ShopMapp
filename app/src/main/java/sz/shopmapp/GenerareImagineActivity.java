cab31a3bb1ccb08991c0b9f0cf12b5b261fe1786
ZuzUBA <tavi2303@yahoo.com>
2017-04-23 13:25:25 +0300

    creat clasa PrelucrareImagine si metoda Prelucrare in care sa modificam imaginea pentru afisare

--- app/src/main/java/sz/shopmapp/GenerareImagineActivity.java
+++ app/src/main/java/sz/shopmapp/GenerareImagineActivity.java
@@ -1,6 +1,7 @@
 package sz.shopmapp;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
@@ -10,7 +11,9 @@ public class GenerareImagineActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generare_imagine);
+
+        PrelucrareImagine pi = new PrelucrareImagine(BitmapFactory.decodeResource(getResources(), R.drawable.magazin));
         ZoomableImageView touch = (ZoomableImageView) findViewById(R.id.imageEnhance);
-        touch.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.magazin));
+        touch.setImageBitmap(pi.Prelucrare());
     }
 }
---
+++ app/src/main/java/sz/shopmapp/PrelucrareImagine.java
@@ -0,0 +1,24 @@
+package sz.shopmapp;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.util.Log;
+
+/**
+ * Created by tavi2 on 22-04-2017.
+ */
+
+public class PrelucrareImagine {
+    private Bitmap initialImage;
+
+    public PrelucrareImagine(Bitmap b){
+        this.initialImage = b;
+    }
+
+    public Bitmap Prelucrare(){
+        Bitmap bm = initialImage;       //prelucrare pe bm si returnare imagine pentru afisat
+
+
+        return bm;
+    }
+}
